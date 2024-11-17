057d9ed0a5c03dd00a7597785cb3576793b0384a
Kavini Weerasinghe <131370963+kavini9@users.noreply.github.com>
2024-11-18 01:50:03 +0200

    Update pipex.c

--- pipex.c
+++ pipex.c
@@ -27,18 +27,6 @@ int	main(int ac, char **av, char **envp)
 }
 
 
-void pipex_init(t_pipex *pipex, int ac, char **av, char **envp)
-{
-	pipex -> infd = open(av[1], O_RDONLY);
-	if (argc > 5 && ft_strncmp(av[1], "here_doc", 8) == 0)
-		pipex -> heredoc = 1;
-	pipex -> cmd_count = ac - 3 - pipex -> heredoc;
-	pipex -> outfd = open(av[ac - 1], O_CREAT | O_RDWR | O_TRUNC, 0644);
-	pipex -> arr_path = get_path_array(envp);
-	pipex -> cmds ;
-	pipex -> pfds = init_pipes();
-
-}
 
 void fd_redirect(int i, t_pipex pipex)
 {
@@ -68,40 +56,10 @@ void pipex(t_pipex pipex)
 	{
 		pid = fork();
 		if (pid < 0)
-			return (perror("Fork: "));
+			pipex_error("Fork: "));
 		else if (pid == 0)
 			execute_cmd(i, pipex);
 	}
 
-}
-
-void execute_cmd(int child, t_pipex pipex)
-{
-	char	**arr_cmd;
-
-	arr_cmd = get_cmd_array();
-
-
-}
-
-char **validate_cmd(t_pipex pipex)
-{
-	
-}
-
-char	**get_arr_path(char **envp)
-{
-	char	**arr_path;
-
-	arr_path = NULL;
-	if (!envp || !*envp)
-		return (NULL); //check what kind of error this may cause
-	while (*envp && ft_strncmp(*envp, "PATH=", 5))
-		envp++;
-	if (*envp)
-		arr_path = ft_split( *envp + 5, ':');
-	if (!arr_path)
-		//erro message cz arr_path is null
-	return (arr_path);
 }
 
