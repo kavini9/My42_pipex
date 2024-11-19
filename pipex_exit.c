c0893959ce19ab8a8870b21ae12f5a44bbb98791
Kavini Weerasinghe <131370963+kavini9@users.noreply.github.com>
2024-11-19 03:39:32 +0200

    Update pipex_exit.c

--- pipex_exit.c
+++ pipex_exit.c
@@ -1,1 +1,22 @@
+void	close_pipe_arr(t_pipex *pipex)
+{
+  int *fd;
 
+  fd = pipex -> pfds;
+	while (*fd)
+  {
+		close(*fd);
+    fd++;
+  }
+	free(pipex -> pfds);
+}
+
+void	pipex_error(const char *err_note, t_pipex *pipex)
+{
+	perror(err_note);
+	close_all_pipes(pipex);
+  free(pipex -> arr_path);
+  free(pipex -> path);
+  free_arr(pipex -> cmd_arr);
+	exit(EXIT_FAILURE);
+}
