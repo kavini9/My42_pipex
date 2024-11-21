1e8ea79fe5766d52dc44c3b9bbb5f61e2161338e
Kavini Weerasinghe <131370963+kavini9@users.noreply.github.com>
2024-11-22 00:39:45 +0200

    Update pipex.c

--- pipex.c
+++ pipex.c
@@ -26,14 +26,12 @@ int	main(int ac, char **av, char **envp)
 	pipex(&pipex);
 }
 
-void	dup_io(t_pipex *data, int infd, int outfd)
+void	dup_io(t_pipex *pipex, int rd_fd, int wr_fd)
 {
-	if (dup2(infd, STDIN) == -1)
-		pipex_error("dup failed);
-	if (dup2(write_fd, STDOUT) == -1)
-		pipex_error("dup failed);
-	close(infd);
-	close(outfd);
+	if (dup2(rd_fd, STDIN) == -1)
+		pipex_error("dup failed");
+	if (dup2(wr_fd, STDOUT) == -1)
+		pipex_error("dup failed");
 }
 
 
@@ -45,8 +43,17 @@ void redirect_io(int i, t_pipex pipex)
 		dup_io(pipex, pipex -> pfds[(i - 1) * 2], pipex -> outfd);
 	else
 		dup_io(pipex, pipex -> pfds[(i - 1) * 2], pipex -> pfds[(i * 2) + 1]);
-	
+	close_pipes(pipex)
+}
 
+int	wait_child(pid_t pid)
+{
+	int	status;
+
+	waitpid(pid, &status, 0);
+	if (WIFEXITED(status))
+		return (WEXITSTATUS(status));
+	return (FAILSTD);
 }
 
 void pipex(t_pipex pipex)
@@ -61,10 +68,15 @@ void pipex(t_pipex pipex)
 		if (pid < 0)
 			pipex_error("Fork: "));
 		else if (pid == 0)
+		{
 			redirect_io(i, pipex);
 			execute_cmd(i, pipex);
+		}
 		i++;
 	}
-
+	close_pfds();
+	while (pid > 0)
+		status = wait_child(pid);
+	return (status);
 }
 
